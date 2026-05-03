20161815c78bffbbb57305669f056c6551b10908
Tilioop <tilio.preto@gmail.com>
2026-05-03 20:49:14 +0200

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB; // Obligatoire pour parler à la BDD
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
-        // On récupère toutes les lignes de ta table 'classe'
         $classes = DB::table('classe')->get();
 
-        // On envoie ces données à la vue 'test'
         return view('test', compact('classes'));
     }
 }
